f28b0cba1326f2411ab3e27cdc1c732852539dc5
Ethan  Grebmeier <ethangrebmeier@gmail.com>
2021-05-16 14:23:52 -0600

    Updated status response on old access token

--- scripts/getAccessToken.js
+++ scripts/getAccessToken.js
@@ -5,9 +5,8 @@ async function getAccessToken(req, res, next){
         const authHeader = req.headers['authorization']
         const reqAccessToken = authHeader && authHeader.split(' ')[1]
         if (reqAccessToken == null){return res.sendStatus(401)}
-
         jwt.verify(reqAccessToken, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-            if (err) {return res.sendStatus(403)}
+            if (err) {return res.sendStatus(401)}
             req.userId = user.id
             next()
         })
