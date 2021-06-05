f5e1db5c46239f9c8b5d900bca0dbc66108a2479
Ethan Grebmeier <ethangrebmeier@gmail.com>
2021-06-04 22:43:11 -0700

    Changed isComplete to isCompleted

--- schemas/applets/checklist/ChecklistItem.js
+++ schemas/applets/checklist/ChecklistItem.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const ChecklistItemSchema = new mongoose.Schema({
     title: String,
     date: String,
-    isComplete: Boolean,
+    isCompleted: Boolean,
     isChecked: Boolean
 }, {strict: false})
 
