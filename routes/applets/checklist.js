a98ae9925b718a2be36a5f906dfdf85b96c23475
Ethan Grebmeier <ethangrebmeier@gmail.com>
2021-06-04 22:41:55 -0700

    Added applets routes

--- routes/applets/checklist.js
+++ routes/applets/checklist.js
@@ -3,43 +3,84 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     const user = await UserModel.findById(req.userId).lean()
+    console.log(user)
     if (!user?.appletMeta?.checklist) {
         return res.sendStatus(404)
     }
     return res.send(user.appletMeta.checklist)
 })
 
-router.post('/', (req, res) => {
-    let {title, isComplete} = req.body.item
-    if (!title || typeof isComplete == 'undefined'){
+router.post('/', async (req, res) => {
+    let item = req.body.item
+    if (!item || !item.title || typeof item.isCompleted == 'undefined'){
         return res.sendStatus(400)
     }
-    const user = UserModel.findByIdAndUpdate(
+    const user = await UserModel.findByIdAndUpdate(
         req.userId, 
         { '$push': {'appletMeta.checklist.items': req.body.item}}, 
         {new: true})
     .lean()
 
+    console.log(user)
+
     if (!user?.appletMeta?.checklist) {
         return res.sendStatus(404)
     }
 
-    return res.json(req.body.item)
+    let items = user.appletMeta.checklist.items
+    console.log(items[items.length - 1])
+    console.log(items)
+
+    return res.json(items[items.length - 1])
 })
 
-router.put('/:itemId', (req, res) => {
-    let {title, isComplete} = req.body.item
-    if (!title || typeof isComplete == 'undefined'){
+router.put('/:itemId', async (req, res) => {
+    let item = req.body.item
+    console.log(item)
+    if (!item.title || typeof item.isCompleted == 'undefined'){
         return res.sendStatus(400)
     }
-    const user = UserModel.findOneAndUpdate({
+    const user = await UserModel.findOneAndUpdate({
         _id: req.userId,
-        'appletMeta.checklist.items._id': itemId
-    }, req.body.item, {new: true}).lean()
+        'appletMeta.checklist.items._id': req.params.itemId
+    }, 
+    {
+        '$set' : {
+            'appletMeta.checklist.items.$' : item
+        }
+    }, {new: true}).lean()
     if (!user?.appletMeta?.checklist) {
         return res.sendStatus(404)
     }
     return res.json(item)
 })
+
+router.delete('/', async (req, res) => {
+    const user = await UserModel.findByIdAndUpdate(req.userId,
+    {
+        $pull: {'appletMeta.checklist.items': {isCompleted: true}}
+    }).lean()
+    if (!user?.appletMeta?.checklist) {
+        return res.sendStatus(404)
+    }
+    return res.send(user.appletMeta.checklist)
+})
+
+router.delete('/:itemId', async (req, res) => {
+    console.log(req.params.itemId)
+    const user = await UserModel.findOneAndUpdate({
+        _id: req.userId
+    },
+    {
+        $pull: {'appletMeta.checklist.items': {_id: req.params.itemId}}
+    }).lean()
+    if (!user?.appletMeta?.checklist) {
+        return res.sendStatus(404)
+    }
+    return res.send(user.appletMeta.checklist)
+})
+
+
+module.exports = router
---
+++ routes/applets/notepad.js
@@ -0,0 +1,44 @@
+const UserModel = require('../../schemas/User')
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/', async (req, res) => {
+    const user = await UserModel.findById(req.userId).lean()
+    console.log(user)
+    if (!user?.appletMeta?.notepad) {
+        return res.sendStatus(404)
+    }
+    return res.send({notepad: user.appletMeta.notepad})
+})
+
+router.post('/', async (req, res) => {
+
+    let parsedNotepad = {
+        title: req.body.notepad.title,
+        text: req.body.notepad.text 
+    }
+
+    console.log(parsedNotepad)
+
+    const user = await UserModel.findByIdAndUpdate(
+        req.userId, 
+        {
+            'appletMeta.notepad' : parsedNotepad
+        }, 
+        {new: true})
+    .lean()
+    console.log('Notepad Update')
+    console.log('')
+    console.log(user)
+
+    if (!user?.appletMeta?.notepad) {
+        return res.sendStatus(404)
+    }
+
+    return res.json({notepad: parsedNotepad})
+})
+
+
+
+module.exports = router
